add2d42a111a3c89815737dcd37ce9488765303b
Okoyev <okoye4045@gmail.com>
2022-04-08 19:49:27 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int z = 0;
+	
+	for (z = 0; z < 10 ; z++)
+	{
+		putchar(z + '0');
+	}
+	putchar('\n');
+	return (0);
+}
