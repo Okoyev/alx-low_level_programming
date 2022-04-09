a5424f22bee43ad0a1bff43e3bd1d3d6d1985467
Okoyev <okoye4045@gmail.com>
2022-04-08 19:41:31 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int a = o;
+
+	while (a < 10)
+	{
+		printf("%i", a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
