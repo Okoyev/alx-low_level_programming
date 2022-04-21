adc6ec162ecc15454cb6c4b66a93c4b6db622843
Okoyev <okoye4045@gmail.com>
2022-04-20 20:43:28 -0700

    batch 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * resetto98 _ updates value that it to 98
+ * reset_to_98 - updates value that it to 98
  * @n: the pointer
  */
 void reset_to_98(int *n)
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*&
+ * _puts - prints a string followed by anew line
+ * @str: char type pointer
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
